585ab8685ab396c6e24fd6bc133bbb522146d0cf
Brandon Beierle <bbeierle21@gmail.com>
2025-07-09 04:29:26 +0000

    module.exports = {   allowedDevOrigins: [     // local dev     'http://l

---
+++ frontend/genkit.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  allowedDevOrigins: [
+    // local dev
+    'http://localhost:9002',
+    // Cloud Workstations URL
+    'https://6000-firebase-studio-*.cloudworkstations.dev'
+  ]
+}
